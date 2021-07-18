412d1383de51b5a544ca1c5f79ca7a037b4881ba
zzj <2461927976@qq.com>
2021-07-18 18:20:04 +0800

    页面骨架完成

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <MHeader></MHeader>
-    <!-- <router-view /> -->
+    <Tab></Tab>
+     <router-view />
   </div>
 </template>
 
 <script>
-import MHeader from "./components/m-header/m-header.vue"
+import MHeader from "./components/m-header/m-header"
+import Tab from "./components/tab/tab"
 export default {
   name: "App",
   components:{
-    MHeader
+    MHeader,
+    Tab
   }
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,37 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import Rank from "@/components/rank/rank";
+import Search from "@/components/search/search";
+import Recommend from "@/components/recommend/recommend";
+import Singer from "@/components/singer/singer";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: "/",
+      redirect: "/recommend"
+    },
+    {
+      path: "/recommend",
+      name: "Recommend",
+      component: Recommend
+    },
+    {
+      path: "/singer",
+      name: "Singer",
+      component: Singer
+    },
+    {
+      path: "/search",
+      name: "Search",
+      component: Search
+    },
+    {
+      path: "/rank",
+      name: "Rank",
+      component: Rank
     }
   ]
-})
+});
